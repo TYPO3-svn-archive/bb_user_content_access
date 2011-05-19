ec3938e769de054cd6dd63773dffccc2ac1185bc
bunse <bunse@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-19 08:57:59 +0000

    1.0.0 RELEASE * ext_emconf.php has been cleaned by EM
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/bb_user_content_access/trunk@47894 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "bb_user_content_access".
 #
-# Auto generated 18-05-2011 17:45
+# Auto generated 19-05-2011 10:56
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'alpha',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -39,7 +39,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"3855";s:10:"README.txt";s:4:"ee2d";s:29:"class.ux_t3lib_pageSelect.php";s:4:"f63e";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"1896";s:14:"ext_tables.php";s:4:"a9e2";s:14:"ext_tables.sql";s:4:"6a37";s:16:"locallang_db.xml";s:4:"928b";s:19:"doc/wizard_form.dat";s:4:"623d";s:20:"doc/wizard_form.html";s:4:"6780";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"da4b";s:10:"README.txt";s:4:"ee2d";s:29:"class.ux_t3lib_pageSelect.php";s:4:"6271";s:12:"ext_icon.gif";s:4:"5bb3";s:17:"ext_localconf.php";s:4:"1896";s:14:"ext_tables.php";s:4:"321f";s:14:"ext_tables.sql";s:4:"8fe5";s:16:"locallang_db.xml";s:4:"fd7c";s:19:"doc/wizard_form.dat";s:4:"623d";s:20:"doc/wizard_form.html";s:4:"6780";}',
+	'suggests' => array(
+	),
 );
 
 ?>
